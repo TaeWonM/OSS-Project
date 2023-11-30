ba3a1b281c0d9e633747df1841d7a11974bbac55
coonycoony <kimsungsu1200@gmail.com>
2023-11-30 23:18:03 +0900

    코드 자체의 전반적인 수정

--- 1-Stage.c
+++ 1-Stage.c
@@ -1,9 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <windows.h>
 #include <conio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <windows.h>
 
 #define XPOS 50
 #define YPOS 5
@@ -13,21 +13,7 @@
 #define DOWN 80
 #define MAX_SIZE1 12
 #define MAX_SIZE2 15
-#define MAX_SIZE3 20
-
-typedef struct {
-    int row;
-    int col;
-} Ghost;
-
-Ghost ghosts[MAX_SIZE3 * MAX_SIZE3];
-int num_ghosts = 1;
-
-Ghost initial_ghost_positions[][3] = {
-    {{7, 7}},           // 레벨 1
-    {{5, 5}, {15, 15}}, // 레벨 2
-    {{5, 5}, {15, 15}, {10, 10}} // 레벨 3
-};
+#define MAX_SIZE3 21
 
 int MAX_SIZE;
 int flag[MAX_SIZE3][MAX_SIZE3] = { 1 };
@@ -37,18 +23,26 @@ int ghost_row = 7, ghost_col = 7;
 int game_timer;
 int game_level = 0;
 time_t start_time;
+int num_ghosts;  // 유령의 수
 
+typedef struct {
+    int row;
+    int col;
+    int direction;
+} Ghost;
+
+Ghost ghosts[MAX_SIZE3 * MAX_SIZE3];
+
+void moveGhost_player(int player_row, int player_col, int ghost_index);
 void GotoXY(int x, int y);
 void print_mazeGame(char maze[][MAX_SIZE3], int row);
 int p_block(char maze[][MAX_SIZE3], int row, int col);
-int m_block(char maze[][MAX_SIZE3], int row, int col);
+int m_block_r(char maze[][MAX_SIZE3], int row, int col);
+int m_block_p(char maze[][MAX_SIZE3], int row, int col);
 void move_maze(char maze[][MAX_SIZE3], int* row, int* col);
-void moveGhost_player(int player_row, int player_col);
-void moveGhost_random(int ghost_index);
 void CursorView(char show);
 void printTimeElapsed();
 int fileopen();
-void addGhost();
 void initializeGhosts();
 
 int main(void) {
@@ -60,23 +54,24 @@ int main(void) {
     switch (game_level) {
     case 1:
         MAX_SIZE = MAX_SIZE1;
-        num_ghosts = 1;
         game_timer = 60;
+        num_ghosts = 1;
         break;
     case 2:
         MAX_SIZE = MAX_SIZE2;
-        num_ghosts = 2;
         game_timer = 60;
+        num_ghosts = 2;
         break;
     case 3:
         MAX_SIZE = MAX_SIZE3;
+        game_timer = 120;
         num_ghosts = 3;
-        game_timer = 60;
         break;
     default:
         printf("잘못된 입력.\n");
         return 1;
     }
+
     fileopen();
     CursorView(0);
     time(&start_time);
@@ -90,15 +85,12 @@ int main(void) {
         if (game_level >= 1) {
             for (int i = 0; i < num_ghosts; i++) {
                 if (game_level == 1)
-                    moveGhost_player(row, col);
+                    moveGhost_player(row, col, i);
                 else if (game_level == 2) {
-                    moveGhost_random(i);
-                    moveGhost_player(row, col);
+                    moveGhost_player(row, col, i);
                 }
                 else if (game_level == 3) {
-                    moveGhost_random(i);
-                    moveGhost_random(i + num_ghosts);
-                    moveGhost_player(row, col);
+                    moveGhost_player(row, col, i);
                 }
             }
         }
@@ -137,10 +129,6 @@ int fileopen() {
     }
     fclose(fp);
 
-    if (game_level > 1) {
-        addGhost();
-    }
-
     return 0;
 }
 
@@ -167,7 +155,7 @@ int GetKey() {
 
     return 0;
 }
-    
+
 void print_mazeGame(char maze[][MAX_SIZE3], int row) {
     for (int i = 0; i < row; i++) {
         GotoXY(XPOS, YPOS + i);
@@ -197,7 +185,7 @@ int p_block(char maze[][MAX_SIZE3], int i, int j) {
         clear_count = 104;
         break;
     case 3:
-        clear_count = 3;
+        clear_count = 200;
         break;
     }
 
@@ -284,158 +272,53 @@ void move_maze(char maze[][MAX_SIZE3], int* row, int* col) {
     }
 }
 
-void moveGhost_player(int player_row, int player_col) {
+void moveGhost_player(int player_row, int player_col, int ghost_index)
+{
     int random_direction = rand() % 4;
 
-    int new_ghost_row = ghost_row;
-    int new_ghost_col = ghost_col;
-
-    switch (random_direction) {
+    switch (random_direction)
+    {
     case 0:
-        if (!(m_block_p(maze, ghost_row - 1, ghost_col))) {
-            if (maze[ghost_row][ghost_col] != '*')
-                maze[ghost_row][ghost_col] = '0';
+        if (!(m_block_p(maze, ghosts[ghost_index].row - 1, ghosts[ghost_index].col)))
+        {
+            if (maze[ghosts[ghost_index].row - 1][ghosts[ghost_index].col] != '*')
+                maze[ghosts[ghost_index].row][ghosts[ghost_index].col] = '0';
 
-            new_ghost_row--;
-
-            if (new_ghost_row == player_row && new_ghost_col == player_col) {
-                GotoXY(XPOS - 3, YPOS - 2);
-                printf("게임 종료: 유령과 충돌");
-                exit(0);
-            }
-            else if (maze[new_ghost_row][new_ghost_col] != 'x')
-                maze[new_ghost_row][new_ghost_col] = 'y';
-        }
-        break;
-
-    case 1:
-        if (!(m_block_p(maze, ghost_row + 1, ghost_col))) {
-            if (maze[ghost_row][ghost_col] != '*')
-                maze[ghost_row][ghost_col] = '0';
-
-            new_ghost_row++;
-
-            if (new_ghost_row == player_row && new_ghost_col == player_col) {
-                GotoXY(XPOS - 3, YPOS - 2);
-                printf("게임 종료: 유령과 충돌");
-                exit(0);
-            }
-            else if (maze[new_ghost_row][new_ghost_col] != 'x')
-                maze[new_ghost_row][new_ghost_col] = 'y';
-        }
-        break;
-
-    case 2:
-        if (!(m_block_p(maze, ghost_row, ghost_col - 1))) {
-            if (maze[ghost_row][ghost_col] != '*')
-                maze[ghost_row][ghost_col] = '0';
-
-            new_ghost_col--;
-
-            if (new_ghost_row == player_row && new_ghost_col == player_col) {
-                GotoXY(XPOS - 3, YPOS - 2);
-                printf("게임 종료: 유령과 충돌");
-                exit(0);
-            }
-            else if (maze[new_ghost_row][new_ghost_col] != 'x')
-                maze[new_ghost_row][new_ghost_col] = 'y';
-        }
-        break;
-
-    case 3:
-        if (!(m_block_p(maze, ghost_row, ghost_col + 1))) {
-            if (maze[ghost_row][ghost_col] != '*')
-                maze[ghost_row][ghost_col] = '0';
-
-            new_ghost_col++;
-
-            if (new_ghost_row == player_row && new_ghost_col == player_col) {
-                GotoXY(XPOS - 3, YPOS - 2);
-                printf("게임 종료: 유령과 충돌");
-                exit(0);
-            }
-            else if (maze[new_ghost_row][new_ghost_col] != 'x')
-                maze[new_ghost_row][new_ghost_col] = 'y';
-        }
-        break;
-    }
-
-    ghost_row = new_ghost_row;
-    ghost_col = new_ghost_col;
-}
-
-void moveGhost_random(int ghost_index) {
-    int random_direction = rand() % 4;
-
-    int current_ghost_row = ghosts[ghost_index].row;
-    int current_ghost_col = ghosts[ghost_index].col;
-
-    switch (random_direction) {
-    case 0:
-        if (!(m_block_r(maze, current_ghost_row - 1, current_ghost_col))) {
-            if (maze[current_ghost_row - 1][current_ghost_col] != '*')
-                maze[current_ghost_row][current_ghost_col] = '0';
-
+            maze[ghosts[ghost_index].row - 1][ghosts[ghost_index].col] = 'y';
             ghosts[ghost_index].row--;
-
-            if (ghosts[ghost_index].row == ghost_row && ghosts[ghost_index].col == ghost_col) {
-                GotoXY(XPOS - 3, YPOS - 2);
-                printf("게임 종료: 유령과 충돌");
-                exit(0);
-            }
-            else if (maze[ghosts[ghost_index].row][ghosts[ghost_index].col] != 'x')
-                maze[ghosts[ghost_index].row][ghosts[ghost_index].col] = 'y';
         }
         break;
 
     case 1:
-        if (!(m_block_r(maze, current_ghost_row + 1, current_ghost_col))) {
-            if (maze[current_ghost_row + 1][current_ghost_col] != '*')
-                maze[current_ghost_row][current_ghost_col] = '0';
+        if (!(m_block_p(maze, ghosts[ghost_index].row + 1, ghosts[ghost_index].col)))
+        {
+            if (maze[ghosts[ghost_index].row + 1][ghosts[ghost_index].col] != '*')
+                maze[ghosts[ghost_index].row][ghosts[ghost_index].col] = '0';
 
+            maze[ghosts[ghost_index].row + 1][ghosts[ghost_index].col] = 'y';
             ghosts[ghost_index].row++;
-
-            if (ghosts[ghost_index].row == ghost_row && ghosts[ghost_index].col == ghost_col) {
-                GotoXY(XPOS - 3, YPOS - 2);
-                printf("게임 종료: 유령과 충돌");
-                exit(0);
-            }
-            else if (maze[ghosts[ghost_index].row][ghosts[ghost_index].col] != 'x')
-                maze[ghosts[ghost_index].row][ghosts[ghost_index].col] = 'y';
         }
         break;
 
     case 2:
-        if (!(m_block_r(maze, current_ghost_row, current_ghost_col - 1))) {
-            if (maze[current_ghost_row][current_ghost_col - 1] != '*')
-                maze[current_ghost_row][current_ghost_col] = '0';
+        if (!(m_block_p(maze, ghosts[ghost_index].row, ghosts[ghost_index].col - 1)))
+        {
+            if (maze[ghosts[ghost_index].row][ghosts[ghost_index].col - 1] != '*')
+                maze[ghosts[ghost_index].row][ghosts[ghost_index].col] = '0';
 
+            maze[ghosts[ghost_index].row][ghosts[ghost_index].col - 1] = 'y';
             ghosts[ghost_index].col--;
-
-            if (ghosts[ghost_index].row == ghost_row && ghosts[ghost_index].col == ghost_col) {
-                GotoXY(XPOS - 3, YPOS - 2);
-                printf("게임 종료: 유령과 충돌");
-                exit(0);
-            }
-            else if (maze[ghosts[ghost_index].row][ghosts[ghost_index].col] != 'x')
-                maze[ghosts[ghost_index].row][ghosts[ghost_index].col] = 'y';
         }
         break;
 
     case 3:
-        if (!(m_block_r(maze, current_ghost_row, current_ghost_col + 1))) {
-            if (maze[current_ghost_row][current_ghost_col + 1] != '*')
-                maze[current_ghost_row][current_ghost_col] = '0';
+        if (!(m_block_p(maze, ghosts[ghost_index].row, ghosts[ghost_index].col + 1)))
+        {
+            if (maze[ghosts[ghost_index].row][ghosts[ghost_index].col + 1] != '*')
+                maze[ghosts[ghost_index].row][ghosts[ghost_index].col] = '0';
 
+            maze[ghosts[ghost_index].row][ghosts[ghost_index].col + 1] = 'y';
             ghosts[ghost_index].col++;
-
-            if (ghosts[ghost_index].row == ghost_row && ghosts[ghost_index].col == ghost_col) {
-                GotoXY(XPOS - 3, YPOS - 2);
-                printf("게임 종료: 유령과 충돌");
-                exit(0);
-            }
-            else if (maze[ghosts[ghost_index].row][ghosts[ghost_index].col] != 'x')
-                maze[ghosts[ghost_index].row][ghosts[ghost_index].col] = 'y';
         }
         break;
     }
@@ -450,16 +333,10 @@ void printTimeElapsed() {
     printf("남은 시간: %.0lf초", game_timer - elapsed_time);
 }
 
-void addGhost() {
-    for (int i = 0; i < num_ghosts; i++) {
-        ghosts[i].row = initial_ghost_positions[game_level - 1][i].row;
-        ghosts[i].col = initial_ghost_positions[game_level - 1][i].col;
-    }
-}
-
 void initializeGhosts() {
     for (int i = 0; i < num_ghosts; i++) {
-        ghosts[i].row = initial_ghost_positions[game_level - 1][i].row;
-        ghosts[i].col = initial_ghost_positions[game_level - 1][i].col;
+        ghosts[i].row = ghost_row;
+        ghosts[i].col = ghost_col;
+        ghosts[i].direction = 0;
     }
 }
