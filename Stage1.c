50469c919606bea84127e3840c0f046d57d9b423
coonycoony <kimsungsu1200@gmail.com>
2023-12-06 15:35:27 +0900

    먹은 개수 문구 추가

--- Stage1.c
+++ Stage1.c
@@ -89,7 +89,7 @@ char *stage1(int diffi) {
             while (1) if (GetAsyncKeyState(VK_RETURN)) return achivemant;
         }
         GotoXY(80, 10);
-        printf ("%d", clear_count - count);
+        printf ("먹은 개수 : %d", count);
         print_mazeGame(maze, MAX_SIZE);
         move_maze(maze, &row, &col);
 
